92aee84501b11df6247913cbdc37661a46cfa6fa
Bruhno2006 <brunoamadi8@gmail.com>
2025-11-14 17:29:29 +0000

    #3 Sorted out Segmentation Fault Error, also included proper File Checks to Console

--- fileCommands.c
+++ fileCommands.c
@@ -5,11 +5,11 @@
 void commands() {
     printf("\n\n");
     printf("-- COMMANDS --\n\n");
-    printf("Open commands list (commands) (c)\n");
-    printf("Open specified file (open) (op)\n");
+    printf("Show commands list (commands) (co)\n");
+    printf("Open a file (open) (op)\n");
     printf("Read the file (read) (r)\n");
     printf("Write on the file (write) (w)\n");
-    printf("Overwrite the file (overwrite) (w+)\n");
-    printf("Close the file (close) (c)\n");
+    printf("Overwrite the file (overwrite) (ow)\n");
+    printf("Close the file (close) (cl)\n");
     printf("Exit the file system (exit) (e)\n\n\n");
 }
--- fileConsole.c
+++ fileConsole.c
@@ -16,24 +16,42 @@ int console(FILE *fpointer) {
     size_t index;
 
     printf("-- WELCOME TO THE C-PROGRAM FILE SYSTEM -- \n");
-    printf("Go to commands (commands) (c) to find command list\n\n");
+    printf("Go to commands (commands) (co) to find command list\n\n");
 
     while (true) {
         printf("> ");
         fgets(userInput, sizeof(userInput), stdin);
 
-        if (strcmp(userInput, "commands\n") == 0 || strcmp(userInput, "c\n") == 0) {
+        if (strcmp(userInput, "commands\n") == 0 || strcmp(userInput, "co\n") == 0) {
             commands();
         } else if (strcmp(userInput, "read\n") == 0 || strcmp(userInput, "r\n") == 0) {
-            fp = change_handling(fp, "r");
-            read(fp);
+            if (fp == NULL) {
+                printf("\nThere is no file opened to read.\n\n");
+            } else {
+                fp = change_handling(fp, "r");
+                read(fp);
+            }
         } else if (strcmp(userInput, "write\n") == 0 || strcmp(userInput, "w\n") == 0) {
-            fp = change_handling(fp, "w");
-            write(fp, "w");
-        } else if (strcmp(userInput, "close\n") == 0) {
-            fclose(fp);
-        } else if (strcmp(userInput, "open\n") == 0) {
-            fclose(fp);
+            if (fp == NULL) {
+                printf("\nThere is no file opened to write.\n\n");
+            } else {
+                fp = change_handling(fp, "w");
+                write(fp, "w");
+            }
+        } else if (strcmp(userInput, "close\n") == 0 || strcmp(userInput, "cl\n") == 0) {
+            if (fp == NULL) {
+                printf("\nThere is no file opened to close.\n\n");
+            } else {
+                printf("\nClosing %s file now...\n\n", getFile());
+                fclose(fp);
+                fp = NULL;
+            }
+        } else if (strcmp(userInput, "open\n") == 0 || strcmp(userInput, "op\n") == 0) {
+            if (fp != NULL) {
+                printf("close");
+                fclose(fp);
+                fp = NULL;
+            }
             printf("Type out the filename: ");
             fgets(userInput, sizeof(userInput), stdin);
             if (userInput != NULL) {      
@@ -41,15 +59,24 @@ int console(FILE *fpointer) {
                 userInput[index] = '\0';
                 fp = open(userInput);
             } else {
-                printf("\nCan't create a file with NULL.\n");
+                printf("\nCan't create a file with NULL.\n\n");
             }
         } else if (strcmp(userInput, "overwrite\n") == 0 || strcmp(userInput, "ow\n") == 0) {
-            fp = change_handling(fp, "w+");
-            write(fp, "w+");
+            if (fp == NULL) {
+                printf("\nThere is no file opened to overwrite\n\n");
+            } else {
+                fp = change_handling(fp, "w+");
+                write(fp, "w+");
+            }
         } else if (strcmp(userInput, "exit\n") == 0 || strcmp(userInput, "e\n") == 0) {
+            if (fp != NULL) {
+                printf("\nClosing %s file now...\n\n", getFile());
+                fclose(fp);
+            }
+            printf("\nClosing File System...\n\n");
             break;
         } else {
-            printf("Command unrecognisable\n");
+            printf("\nCommand unrecognisable\n\n");
         }
     }
 }
